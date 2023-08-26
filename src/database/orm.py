03749236bcd9adfd63e17316fe63bbdcc9fe6503
satoshi25 <81741466+satoshi25@users.noreply.github.com>
2023-08-26 11:27:52 +0900

    modify: Add view column to article table - article 테이블에 view 조회수 열 추가 - requirements.txt에 pydantic 버전과의 호환성을 위해 sqlalchemy 버전 수정

--- src/database/orm.py
+++ src/database/orm.py
@@ -11,7 +11,9 @@ class Article(Base):
     content = Column(String(1500), nullable=True)
     journal = Column(String(50), nullable=False)
     url = Column(String(2083), nullable=False)
+    view = Column(Integer, nullable=False)
     publish_date = Column(Date, nullable=False)
 
     def __repr__(self):
-        return f"Article(id={self.id}, title={self.title}, content={self.content}, journal={self.journal}, url={self.url}, publish_date={self.publish_date})"
+        return (f"Article(id={self.id}, title={self.title}, content={self.content}, "
+                f"journal={self.journal}, url={self.url}, publish_date={self.publish_date})")
