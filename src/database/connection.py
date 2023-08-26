a55281e1ed940377e0f6cd7c3da2d5404405cd74
satoshi25 <81741466+satoshi25@users.noreply.github.com>
2023-08-26 10:52:33 +0900

    feat: Add session Object - connection에 데이터베이스 상호작용을 위한 session 생성 메소드 추가 - repository에 데이터베이스 조회를 위한 class와 조회 메소드 추가

--- src/database/connection.py
+++ src/database/connection.py
@@ -5,3 +5,12 @@ DATABASE_URL = "mysql+pymysql://root:fomo@127.0.0.1:3306/fomo"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    session = SessionFactory()
+
+    try:
+        yield session
+    finally:
+        session.close()
---
+++ src/database/repository.py
@@ -0,0 +1,21 @@
+from fastapi import Depends
+from typing import List
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from datetime import date
+
+from database.connection import get_db
+from database.orm import Article
+
+
+class ArticleRepository:
+    def __init__(self, session: Session = Depends(get_db)):
+        self.session = session
+
+    def get_rank_articles(self, publish_date: date) -> List[Article]:
+
+        articles: List[Article] | None = list(
+            self.session.scalars(select(Article).where(Article.publish_date == publish_date))
+        )
+
+        return articles
