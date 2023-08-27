2723c428d050c3baf437eedf5518cbb878deb9f8
satoshi25 <81741466+satoshi25@users.noreply.github.com>
2023-08-27 14:51:00 +0900

    feat: Add get_journal_rank_1_articles_handler API with GET method - 해당날짜에 각 journal별 조회수 1위 조회하는 API 추가 - repository에 조회를 위한 메소드 추가

--- src/database/repository.py
+++ src/database/repository.py
@@ -26,3 +26,39 @@ class ArticleRepository:
             )
         )
         return articles
+
+    def get_rank_journal_rank_1_articles(self, publish_date: date) -> List[Article] | None:
+
+        sub_article = (
+            select(
+                Article,
+                func.row_number().over(
+                    partition_by=Article.journal,
+                    order_by=desc(Article.view)
+                ).label('rank')
+            ).where(Article.publish_date == publish_date).subquery()
+        )
+
+        stmt = select(sub_article).where(sub_article.c.rank == 1).order_by(sub_article.c.view.desc())
+
+        articles: Sequence[Row] | None = self.session.execute(stmt).fetchall()
+        # articles 의 타입이 <class 'sqlalchemy.engine.row.Row'>를 담은 list 다.
+        # 요소는 tuple 형태다.
+
+        if not articles:
+            articles = None
+        else:
+            articles = [
+                Article(
+                    id=article[0],
+                    title=article[1],
+                    content=article[2],
+                    journal=article[3],
+                    url=article[4],
+                    view=article[5],
+                    publish_date=article[6]
+                )
+                for article in articles
+            ]
+
+        return articles
--- src/main.py
+++ src/main.py
@@ -57,3 +57,21 @@ def get_rank_journal_articles_handler(
             )[:10]
         ]
     )
+
+
+@app.get("/articles/{publish_date}/journal-rank-1")
+def get_journal_rank_1_articles_handler(
+    publish_date: str,
+    article_repo: ArticleRepository = Depends(),
+) -> ArticleListRankSchema:
+
+    publish_date: date = date.fromisoformat(publish_date)
+    articles: List[Article] | None = article_repo.get_rank_journal_rank_1_articles(publish_date=publish_date)
+    if not articles:
+        raise HTTPException(status_code=404, detail="Articles Not Found")
+
+    return ArticleListRankSchema(
+        articles=[
+            ArticleSchema.from_orm(article) for article in articles
+        ]
+    )
